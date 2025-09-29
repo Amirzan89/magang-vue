9005964fe5c1e408265d3bfe73356893ae2954b2
Amirzan Fikri Prasetyo <102281081+Amirzan89@users.noreply.github.com>
2025-09-29 09:24:01 +0700

    almost done responsive event detail

--- src/views/EventDetail.vue
+++ src/views/EventDetail.vue
@@ -6,7 +6,7 @@ import 'swiper/css'
 import 'swiper/css/navigation'
 import "swiper/css/thumbs"
 import 'swiper/css/autoplay'
-import { reactive, onBeforeMount, defineComponent, useSlots, h, type ComponentPublicInstance, ref } from 'vue'
+import { reactive, onBeforeMount, defineComponent, useSlots, h, type ComponentPublicInstance, ref, computed } from 'vue'
 import { RouterLink, useRoute } from 'vue-router'
 import { breakpoints } from '@/composables/useScreenSize'
 import { useFetchDataStore } from '@/stores/FetchData'
@@ -36,17 +36,23 @@ const onMainSwiper = (swiper: SwiperType) => {
             thumbsSwiper.value.slideTo(swiper.realIndex);
         }
     });
-};
+}
 const handleMouseEnter = () => {
     if(mainSwiper.value?.autoplay){
         mainSwiper.value.autoplay.stop();
     }
-};
+}
 const handleMouseLeave = () => {
     if(mainSwiper.value?.autoplay){
         mainSwiper.value.autoplay.start();
     }
-};
+}
+const slidePerView = computed(() => {
+    if(breakpoints.greater('lg').value){
+        return 5
+    }
+    return 3
+})
 const cleanImg = (img: any) => img.replaceAll('"', '').trim();
 onBeforeMount(async() =>{
     const res = (await fetchDataS.fetchPage(useRoute().path, {}))
@@ -78,11 +84,11 @@ const metaDataLoading = {
 }
 </script>
 <template>
-    <section class="w-[95%] mt-10 mx-auto flex flex-col sm:flex-row gap-5">
-        <div class="relative sm:w-[50%] lg:w-[45%] xl:w-[40%]">
+    <section class="w-[90%] lg:w-[95%] mt-3 lg:mt-10 mx-auto flex flex-col md:flex-row gap-5">
+        <div class="relative md:w-[50%] lg:w-[45%] xl:w-[45%]">
             <div v-if="local.detail_event && local.detail_event.img && local.detail_event.img.length > 0">
-                <Swiper :modules="[Navigation, Thumbs, Autoplay]" :thumbs="{ swiper: thumbsSwiper }" :space-between="10" :navigation="{ nextEl: '.btn-next', prevEl: '.btn-prev' }" :loop="true" :autoplay="{ delay: 30000, disableOnInteraction: false }" @swiper="onMainSwiper" class="relative  h-[400px] mb-3 group" @mouseenter="handleMouseEnter" @mouseleave="handleMouseLeave">
-                    <template v-for="(img, i) in local.detail_event.img.filter(x => x && x !== '-')" :key="i">
+                <Swiper :modules="[Navigation, Thumbs, Autoplay]" :thumbs="{ swiper: thumbsSwiper }" :space-between="10" :navigation="{ nextEl: '.btn-next', prevEl: '.btn-prev' }" :loop="true" :autoplay="{ delay: 30000, disableOnInteraction: false }" @swiper="onMainSwiper" class="relative  h-[200px] phone:!h-[250px] sm:!h-[300px] md:!h-[350px] lg:!h-[400px] xl:h-[450px] mb-3 group" @mouseenter="handleMouseEnter" @mouseleave="handleMouseLeave">
+                    <template v-for="(img, i) in local.detail_event.img.filter((x: any) => x && x !== '-')" :key="i">
                         <SwiperSlide>
                             <img v-if="img && (img !== '-')" :src="img" class="w-full h-full object-cover" />
                         </SwiperSlide>
@@ -92,61 +98,58 @@ const metaDataLoading = {
                         <I_VRight class="btn-next size-8 text-black-500">next</I_VRight>
                     </div>
                 </Swiper>
-                <Swiper :modules="[Navigation, Thumbs, Autoplay]" @Swiper="(swiper) => (thumbsSwiper = swiper)" :space-between="0" :slides-per-view="3" centered-slides watch-slides-progress class="w-4/5 h-[100px]">
+                <Swiper :modules="[Navigation, Thumbs, Autoplay]" @Swiper="(swiper) => (thumbsSwiper = swiper)" :space-between="0" :slides-per-view="slidePerView" centered-slides watch-slides-progress class="w-[63%] xs:w-[54%] phone:w-[60%] sm:w-[45%] lg:w-[78%] xl:w-4/5 h-[65px] xs:h-[70px] phone:h-[80px] sm:h-[90px] md:h-[95px] lg:h-[102px] xl:h-[110px] 2xl:h-[120px] bg-red-500"> 
                     <SwiperSlide v-for="(img, i) in local.detail_event.img" :key="i" class="w-fit">
                         <img v-if="cleanImg(img) !== '-'" :src="cleanImg(img)" class="h-full object-contain" />
                     </SwiperSlide>
                 </Swiper>
             </div>
             <div v-else>
-                <Skeleton :pt="{ root: { class: ['!h-[400px] !rounded-lg mb-3'], style: 'background-color: rgba(0,0,0, 0.18)' }}"/>
-                <div class="w-4/5 h-[100px] mx-auto flex gap-5">
-                    <Skeleton :pt="{ root: { class: ['flex-1 !h-full !rounded-sm ]'], style: 'background-color: rgba(0,0,0, 0.18)' }}"/>
-                    <Skeleton :pt="{ root: { class: ['flex-1 !h-full !rounded-md ]'], style: 'background-color: rgba(0,0,0, 0.18)' }}"/>
-                    <Skeleton :pt="{ root: { class: ['flex-1 !h-full !rounded-lg ]'], style: 'background-color: rgba(0,0,0, 0.18)' }}"/>
+                <Skeleton :pt="{ root: { class: ['!h-[200px] phone:!h-[250px] sm:!h-[300px] md:!h-[350px] lg:!h-[400px] xl:!h-[450px] !rounded-lg mb-3'], style: 'background-color: rgba(0,0,0, 0.18)' }}"/>
+                <div class="w-[63%] xs:w-[54%] phone:w-[60%] sm:w-[45%] lg:w-[78%] xl:w-4/5 h-[65px] xs:h-[70px] phone:h-[80px] sm:h-[90px] md:h-[95px] lg:h-[102px] xl:h-[110px] 2xl:h-[120px] mx-auto flex gap-5">
+                    <template v-for="i in slidePerView">
+                        <Skeleton :pt="{ root: { class: ['flex-1 !h-full !rounded-sm ]'], style: 'background-color: rgba(0,0,0, 0.18)' }}"/>
+                    </template>
                 </div>
             </div>
         </div>
-        <div class="bg-amber-500 flex-1">
-            <h3 class="w-fit mt-5 text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-semibold text-[#242565]">All Events</h3>
-            <p class="mt-5 text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, atque! Nihil consequuntur quasi perferendis neque facilis iusto dolore eaque. Ullam pariatur amet dicta voluptatum minima laudantium voluptates, rem quia ex.</p>
-            <!-- <h3>{{ local.detail_event.name_event }}</h3> -->
-            <!-- <h3>{{ local.detail_event.event_description }}</h3> -->
-            <!-- <p>{{ local.detail_event.event_description }}</p> -->
-            <div class="mt-5 flex">
+        <div class="flex-1 h-fit">
+            <h3 class="w-fit mt-1 sm:mt-3 lg:mt-5 text-lg sm:text-xl lg:text-2xl xl:text-3xl font-semibold text-[#242565]">All Events</h3>
+            <p class="xl:mt-5 text-base sm:text-lg lg:text-xl xl:text-2xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, atque! Nihil consequuntur quasi perferendis neque facilis iusto dolore eaque. Ullam pariatur amet dicta voluptatum minima laudantium voluptates, rem quia ex.</p>
+            <div class="w-[95%] ml-[5%] mt-5 flex">
                 <div class="flex flex-col">
-                    <I_Location class="w-5 h-5 sm:w-8 sm:h-8 text-black"/>
-                    <I_Date class="w-5 h-5 sm:w-8 sm:h-8 text-black"/>
-                    <I_Ticket class="w-5 h-5 sm:w-8 sm:h-8 text-black"/>
+                    <I_Location class="size-5 xl:size- sm:w-8 sm:h-8 text-black"/>
+                    <I_Date class="size-5 xl:size- sm:w-8 sm:h-8 text-black"/>
+                    <I_Ticket class="size-5 xl:size- sm:w-8 sm:h-8 text-black"/>
                 </div>
-                <div class="flex flex-col">
+                <div class="ml-2 flex flex-col text-sm sm:text-base lg:text-lg xl:text-xl">
                     <p>Location</p>
                     <p>Date</p>
                     <p>Entry</p>
                 </div>
-                <div class="flex flex-col">
+                <div class="ml-3 flex flex-col text-sm sm:text-base lg:text-lg xl:text-xl">
                     <span>:</span>
                     <span>:</span>
                     <span>:</span>
                 </div>
-                <div class="flex flex-col">
-                    <!-- <p>{{ local.detail_event. }}</p>
-                    <p>{{ local.detail_event. }}</p>
+                <div class="ml-1 flex flex-col text-sm sm:text-base lg:text-lg xl:text-xl">
+                    <!-- <p>{{ local.detail_event.nama_lokasi }}</p>
+                    <p>{{ local.detail_event.start_date }}</p>
                     <p>{{ local.detail_event.price }}</p> -->
                 </div>
             </div>
-            <div>
-                <a :href="local.detail_event?.link_event" target="_blank" rel="noopener noreferrer">Book Event</a>
-                <a :href="local.detail_event?.event_detail" target="_blank" rel="noopener noreferrer">Learn More</a>
+            <div class="w-fit relative left-1/2 -translate-x-1/2 mt-5 flex items-center gap-5">
+                <Button variant="outlined" as="a" :href="local.detail_event?.link_event" target="_blank" rel="noopener noreferrer" class="w-fit !text-[#3D37F1] hover:!text-white !border-[#3D37F1] hover:!bg-[#3D37F1] !text-sm sm:!text-base lg:!text-lg xl:!text-xl">Book Event</Button>
+                <Button variant="outlined" as="a" :href="local.detail_event?.event_detail" target="_blank" rel="noopener noreferrer" class="w-fit !text-[#3D37F1] hover:!text-white !border-[#3D37F1] hover:!bg-[#3D37F1] !text-sm sm:!text-base lg:!text-lg xl:!text-xl">Learn More</Button>
             </div>
         </div>
     </section>
     <section class="relative overflow-x-clip">
         <img src="@/assets/images/cele-3.png" alt="" class="absolute bottom-0 -right-[29.5%] w-[75%] h-[75%] -z-1 object-cover opacity-30"/>
-        <div class="w-[80%] mt-5 mx-auto">
-            <div>
-                <h2 class="w-fit mt-5 mx-auto lg:mx-0 text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-semibold text-[#242565]">More Details</h2>
-                <p class="w-[90%] mt-5 mx-auto text-sm sm:text-text-base md:text-lg lg:text-xl xl:text-2xl bg-green-500">
+        <div class="w-[90%] mt-7 xl::mt-5 lg:mt-10 mx-auto">
+            <div class="">
+                <h2 class="w-fit mt- mx-auto lg:mx-0 text-base sm:text-lg lg:text-xl xl:text-2xl font-semibold text-[#242565]">More Details</h2>
+                <p class="lg:mt-2 ml-7 text-sm sm:text-base lg:text-lg xl:text-xl">
                     Whether you're a tech enthusiast, an aspiring innovator, or just curious about the future, ExMo 2025 is the place to be. Dive into a world of futuristic ideas, hands-on demonstrations, groundbreaking innovations, and inspiring tech talks — all under one roof.
                     🌟 What to Expect:
                     ⚙️ Live Demos & Showcases
@@ -162,7 +165,7 @@ const metaDataLoading = {
             <div class="relative flex flex-col mt-40 mb-10 xl:mb-30">
                 <div class="w-[95%] mx-auto">
                     <div class="mx-auto flex flex-col justify-center items-center">
-                        <h2 class="w-fit mx-auto lg:mx-0 text-xl xl:text-3xl font-bold">Popular Events</h2>
+                        <h2 class="w-fit mx-auto lg:mx-0 text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold">Popular Events</h2>
                         <p>see other events</p>
                     </div>
                     <CustomCardWithSkeletonComponent v-if="local.all_events && local.all_events.length > 0" :metaData="metaDataPopuler" :inpData="local.all_events" :paralelRender="1">
