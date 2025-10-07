156a71ad8d744fa782c2696a346e7d3137cee781
Amirzan Fikri Prasetyo <102281081+Amirzan89@users.noreply.github.com>
2025-10-07 08:22:53 +0700

    done add event booked

--- src/router/routes/admin.ts
+++ src/router/routes/admin.ts
@@ -5,4 +5,10 @@ export default [
         component: () => import('@/views/Admin/Dashboard.vue'),
         meta: { layout: 'admin' },
     },
+    {
+        path: '/event-booked',
+        name: 'EventBookedPage',
+        component: () => import('@/views/Admin/EventBooked.vue'),
+        meta: { layout: 'admin' },
+    },
 ]
